719019727dc6884aef5e8018a52b39128298f5ad
RaghushettyK <raghukrishna043@gmail.com>
2025-07-19 17:12:23 +0530

    Update Register.jsx

--- my-app/src/components/Register.jsx
+++ my-app/src/components/Register.jsx
@@ -10,7 +10,7 @@ function Register() {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/register', {
+      const res = await axios.post('https://shop-easy-backend-b3fb.onrender.com/api/auth/register', {
         email,
         password,
       });
